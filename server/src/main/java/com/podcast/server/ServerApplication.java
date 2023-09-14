c1b0798a6e18dcdcb78844a4362e5a2377228d66
phuocloc <phuocloc@tuoitre.com.vn>
2023-09-14 21:57:32 +0700

    end section 7

--- server/src/main/java/com/podcast/server/ServerApplication.java
+++ server/src/main/java/com/podcast/server/ServerApplication.java
@@ -2,13 +2,11 @@ package com.podcast.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
-
+@RefreshScope
 public class ServerApplication {
 
 	public static void main(String[] args) {
